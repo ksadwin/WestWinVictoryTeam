e2a9b30544886fdb187c394c870ea3c94031dc0d
ksadwin <kelly.sadwin.ic@gmail.com>
2015-03-17 09:54:19 -0400

    who's doing what

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -4,6 +4,7 @@ not sure if this file should have code or just be notes
 Let's keep updating this as we find more functions to write!
 """
 
+#Kelly
 def runQueries():
     """
     This function will use the txt files in the item folder
@@ -11,27 +12,32 @@ def runQueries():
     so that they can be run as nnn.nnn, nnn.ltc, ltc.nnn, ltc.ltc
     """
 
+#Jen
 def randomResult():
     """
     Returns all webpages in a random order.
     """
 
+#Kelly
 def getR(item):
     """
     Finds number of relevant webpages for a given item.
     """
-
+    
+#Jen
 def precisionAt(R):
     """
     Finds precision at int R
     (number of relevant webpages found in first R results/R)
     """
 
+#Jen
 def averagePrecision():
     """
     Averages precision at each True Positive result
     """
 
+#Kelly
 def auc():
     """
     At each True Negative, multiply the current True Positive rate
